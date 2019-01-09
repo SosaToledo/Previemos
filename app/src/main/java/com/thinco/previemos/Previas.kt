3d48657c363839dc8d470f9561b2b7298730ea3e
Luciano <luchososa1990@hotmail.com.ar>
2019-01-09 03:04:07 -0300

    Load "Previas"

---
+++ app/src/main/java/com/thinco/previemos/Previas.kt
@@ -0,0 +1,5 @@
+package com.thinco.previemos
+
+class Previas (val id: Int, val publico: Boolean){
+
+}
---
+++ app/src/main/java/com/thinco/previemos/previasDAO.kt
@@ -0,0 +1,27 @@
+package com.thinco.previemos
+
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+import java.util.*
+
+
+class previasDAO() {
+    //TODO instanciar cada vez que se hagan consultas con previas.
+    val database = FirebaseDatabase.getInstance()
+    val myRef = database.getReference("previas")
+
+
+    fun cargarPrevia(previa: Previas){
+        val id: String = previa.id.toString()
+        if (previa.publico){
+            myRef.child("publicas").child(id).setValue(previa)
+        }else{
+            myRef.child("privadas").child(id).setValue(previa)
+        }
+    }
+
+}
